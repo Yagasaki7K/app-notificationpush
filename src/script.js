f746d6930bb2795c43fde28d7d7e375dc67c511f
Anderson Marlon <anderson.alves@cenariocapital.com.br>
2021-06-15 15:41:26 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,11 +1,9 @@
 if (!('serviceWorker' in navigator)) {
     console.log("Service Worker isn't supported on this browser, disable or hide UI.");
-    return;
 }
 
   if (!('PushManager' in window)) {
     console.log("Push isn't supported on this browser, disable or hide UI.");
-    return;
 }
 
 requestPermission();
