cc5521830f7e30a774a01354e9c093a60c9ef7b7
Anderson Marlon <anderson.alves@cenariocapital.com.br>
2021-06-15 11:33:03 -0300

    Update

--- src/firebase-sw.js
+++ src/firebase-sw.js
@@ -1,15 +1,3 @@
-const config = {
-    apiKey: "AIzaSyC8VzkQUdmmxsFEoQPQaE-dbys4MA9Oa1Y",
-    authDomain: "sw-creditcards.firebaseapp.com",
-    projectId: "sw-creditcards",
-    storageBucket: "sw-creditcards.appspot.com",
-    messagingSenderId: "1073108829169",
-    appId: "1:1073108829169:web:eb1569a62c16f533dfba50",
-    measurementId: "G-W50CWYMNT2"
-};
-
-const firebase = firebase.initializeApp(config)
-
 messaging.setBackgroundMessageHandler(function(payload) {
     const title = 'Messaging from Cenário Capital'
     const options = {
