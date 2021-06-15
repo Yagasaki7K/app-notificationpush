3d8b313e52968a99638985454fc6b7de90415956
Anderson Marlon <anderson.alves@cenariocapital.com.br>
2021-06-15 11:43:48 -0300

    Update

--- src/firebase-sw.js
+++ src/firebase-sw.js
@@ -1,7 +1,9 @@
-messaging.setBackgroundMessageHandler(function(payload) {
-    const title = 'Messaging from Cenário Capital'
-    const options = {
-        body: payload.data.status
-    }
-    return self.ServiceWorkerRegistration.showNotification(title, options);
-})
+function messagingFirebase() {
+    messaging.setBackgroundMessageHandler(function(payload) {
+        const title = 'Messaging from Cenário Capital'
+        const options = {
+            body: payload.data.status
+        }
+        return self.ServiceWorkerRegistration.showNotification(title, options);
+    })
+}
--- src/script.js
+++ src/script.js
@@ -1,26 +1,41 @@
-const messaging = firebase.messaging();
-    messaging.onTokenRefresh(function () {
-      messaging.getToken()
-        .then(function (refreshedToken) {
-          console.log('Token refreshed.');
-          console.log(refreshedToken);
-          // // Indicate that the new Instance ID token has not yet been sent to the
-          // // app server.
-          // setTokenSentToServer(false);
-          // // Send Instance ID token to app server.
-          // sendTokenToServer(refreshedToken);
-          // // [START_EXCLUDE]
-          // // Display new Instance ID token and clear UI of all previous messages.
-          // resetUI();
-          // // [END_EXCLUDE]
-        })
-        .catch(function (err) {
-          console.log('Unable to retrieve refreshed token ', err);
-          // showToken('Unable to retrieve refreshed token ', err);
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+
+            const messaging = firebase.messaging();
+            messaging.onTokenRefresh(function () {
+                messaging.getToken()
+                    .then(function (refreshedToken) {
+                        console.log('Token refreshed.');
+                        console.log(refreshedToken);
+                        // // Indicate that the new Instance ID token has not yet been sent to the
+                        // // app server.
+                        // setTokenSentToServer(false);
+                        // // Send Instance ID token to app server.
+                        // sendTokenToServer(refreshedToken);
+                        // // [START_EXCLUDE]
+                        // // Display new Instance ID token and clear UI of all previous messages.
+                        // resetUI();
+                        // // [END_EXCLUDE]
+                        messagingFirebase();
+                    })
+                    .catch(function (err) {
+                        console.log('Unable to retrieve refreshed token ', err);
+                        // showToken('Unable to retrieve refreshed token ', err);
+                    });
+            });
+
+            messaging.onMessage(function (payload) {
+                console.log("Message received. ", payload);
+                // ...
+            });
+
+
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
         });
     });
-
-    messaging.onMessage(function(payload) {
-      console.log("Message received. ", payload);
-      // ...
-    });
+}
