4a8ace7002b39e06b3e10ef1452b177b399d5b48
Anderson Marlon <anderson.alves@cenariocapital.com.br>
2021-06-15 11:13:17 -0300

    Update firebase-sw.js

--- src/firebase-sw.js
+++ src/firebase-sw.js
@@ -1,14 +1,4 @@
-const ref = new Firebase("sw-creditcards.firebaseapp.com")
-
-const config = {
-    apiKey: "AIzaSyC8VzkQUdmmxsFEoQPQaE-dbys4MA9Oa1Y",
-    authDomain: "sw-creditcards.firebaseapp.com",
-    projectId: "sw-creditcards",
-    storageBucket: "sw-creditcards.appspot.com",
-    messagingSenderId: "1073108829169",
-    appId: "1:1073108829169:web:eb1569a62c16f533dfba50",
-    measurementId: "G-W50CWYMNT2"
-  };
+importScripts('./script.js')
 
 firebaseConfig.initializeApp(config)
 
