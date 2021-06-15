7aa125a07c6878877ac5429a655ce1a96d960388
Anderson Marlon <anderson.alves@cenariocapital.com.br>
2021-06-15 11:13:48 -0300

    Revert "Update firebase-sw.js"
    This reverts commit 4a8ace7002b39e06b3e10ef1452b177b399d5b48.

--- src/firebase-sw.js
+++ src/firebase-sw.js
@@ -1,4 +1,14 @@
-importScripts('./script.js')
+const ref = new Firebase("sw-creditcards.firebaseapp.com")
+
+const config = {
+    apiKey: "AIzaSyC8VzkQUdmmxsFEoQPQaE-dbys4MA9Oa1Y",
+    authDomain: "sw-creditcards.firebaseapp.com",
+    projectId: "sw-creditcards",
+    storageBucket: "sw-creditcards.appspot.com",
+    messagingSenderId: "1073108829169",
+    appId: "1:1073108829169:web:eb1569a62c16f533dfba50",
+    measurementId: "G-W50CWYMNT2"
+  };
 
 firebaseConfig.initializeApp(config)
 
