0a8f9e1a997fff9cb97ca96627fc2e47d66a6569
Anderson Marlon <anderson.alves@cenariocapital.com.br>
2021-06-17 13:16:37 -0300

    Update

---
+++ src/notification.js
@@ -0,0 +1,12 @@
+function messageFirebase(permissionResult) {
+    if (permissionResult) {
+    const notification = new Notification('New message from Cenário Capital', {
+        body: 'Hello. Now you gonna receive our notification',
+        icon: 'https://cdn1.iconfinder.com/data/icons/logos-brands-in-colors/231/among-us-player-pink-512.png', 
+    })
+  
+    notification.onclick = () => {
+        window.location.href = 'https://google.com.br'
+        }
+    }
+  }
--- src/script.js
+++ src/script.js
@@ -36,24 +36,10 @@ function requestPermission() {
         } else if (Notification !== 'denied') {
         Notification.requestPermission().then(permission => {
             if (permission === 'granted') {
-            messageFirebase(permissionResult);
+            messageFirebase();
             }
         })
       }
     })
-  }  
-  
-  
-  function messageFirebase(permissionResult) {
-    if (permissionResult) {
-    const notification = new Notification('New message from Cenário Capital', {
-        body: 'Hello. Now you gonna receive our notification',
-        icon: 'https://cdn1.iconfinder.com/data/icons/logos-brands-in-colors/231/among-us-player-pink-512.png', 
-    })
-  
-    notification.onclick = () => {
-        window.location.href = 'https://google.com.br'
-        }
-    }
   }
 }
