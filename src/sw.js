8f5be2f69eb1aebdfd8d964c2dba8b476f8eab3e
Anderson Marlon <anderson.alves@cenariocapital.com.br>
2021-06-17 16:57:10 -0300

    Update

--- src/sw.js
+++ src/sw.js
@@ -2,19 +2,6 @@ self.addEventListener("fetch", function (event) {
     console.log(`Service Worker is ready!`)
 });
 
-function messageFirebase(permissionResult) {
-    if (permissionResult) {
-    const notification = new Notification('New message from Notification Push', {
-        body: 'Hello. Now you gonna receive our notification',
-        icon: 'https://cdn1.iconfinder.com/data/icons/logos-brands-in-colors/231/among-us-player-pink-512.png', 
-    })
-  
-    notification.onclick = () => {
-        window.location.href = 'https://google.com.br'
-        }
-    }
-}
-
 let num = 1;
 
 self.addEventListener('push', function(event) {
