6197721ba437189818833b0df62684be8680445f
Anderson Marlon <anderson.alves@cenariocapital.com.br>
2021-06-17 17:16:19 -0300

    Update

--- src/sw.js
+++ src/sw.js
@@ -12,3 +12,18 @@ self.addEventListener('push', function(event) {
         })
     );
 });
+
+self.addEventListener('sync', function(event) {
+	console.log("sync event", event);
+    if (event.tag === 'syncAttendees') {
+        event.waitUntil(syncAttendees()); // sending sync request
+    }
+});
+
+function syncAttendees(){
+	return update({ url: `https://reqres.in/api/users` })
+    	.then(refresh)
+    	.then((attendees) => self.registration.showNotification(
+    		`${attendees.length} attendees to the PWA Workshop`
+    	))
+}
