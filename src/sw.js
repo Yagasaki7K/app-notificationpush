97e09b7c6300806aa9e27e52281b2e4cd0154262
Anderson Marlon <anderson.alves@cenariocapital.com.br>
2021-06-17 16:34:16 -0300

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,11 @@
-self.addEventListener("fetch", function(event) {
-  console.log(`Service Worker is ready!`)
+self.addEventListener("fetch", function (event) {
+    console.log(`Service Worker is ready!`)
+});
+
+self.addEventListener('push', function (event) {
+    event.waitUntil(
+        self.registration.showNotification('ServiceWorker Cookbook', {
+            body: 'Alea iacta est',
+        })
+    );
 });
